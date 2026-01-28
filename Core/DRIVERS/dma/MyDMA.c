d3808e02f8cff510a3d39e9f16120ce29d3e0dce
Bavin <bavinbav5@gmail.com>
2026-01-28 19:22:32 +0300

    the init func of the dma driver is nearly done and its perfect i only need to clear the dma flags which will containt alot of if statements

--- Core/DRIVERS/dma/MyDMA.c
+++ Core/DRIVERS/dma/MyDMA.c
@@ -1,5 +1,28 @@
 #include "MyDMA.h"
-
-void DMAx_init(DMA_config_t* ptr){
+static inline void DMA_config(DMA_TypeDef *dma){
+	if(dma==DMA1){
+		RCC->AHB1ENR|=(1<<21);
+	}
+	else if(dma==DMA2){
+		RCC->AHB1ENR|=(1<<22);
+	}
+	else return;
+}
+void DMAx_init(DMA_TypeDef *DMAx,DMA_config_t* DMA_CONFIG){
+	DMA_config(DMAx);
+	DMA_CONFIG->DMA_STREAM->CR=0;//clear everything
+	DMA_CONFIG->DMA_STREAM->CR|=(DMA_CONFIG->CHANNEL<<25)|
+								(DMA_CONFIG->PRIORITY<<16)|
+								(DMA_CONFIG->MEMORY_DATA_SIZE<<13)|
+								(DMA_CONFIG->PERIPHERAL_DATA_SIZE<<11)|
+								(1<<10)|//Memory address pointer incremented after each data transfer (increment is done according to MSIZE)
+								(DMA_CONFIG->MODE<<8)|
+								(DMA_CONFIG->DIRECTION<<6);
+	DMA_CONFIG->DMA_STREAM->NDTR=DMA_CONFIG->LENGTH-1;//we dont wanna include null terminator
+	DMA_CONFIG->DMA_STREAM->PAR=0;
+	DMA_CONFIG->DMA_STREAM->PAR=(uint32_t)DMA_CONFIG->PERIPHERAL_ADDR;
+	DMA_CONFIG->DMA_STREAM->M0AR=0;
+	DMA_CONFIG->DMA_STREAM->M0AR=(uint32_t)DMA_CONFIG->MEMORY_ADDR;
+	DMA_CONFIG->DMA_STREAM->M1AR=0;//SINCE we don't use double buffer mode we will ignore second address register
 
 }
--- Core/DRIVERS/dma/MyDMA.h
+++ Core/DRIVERS/dma/MyDMA.h
@@ -3,35 +3,35 @@
 #include "stm32f446xx.h"
 #include <stdint.h>
 typedef enum{
-	PRIOTITY_LOW=1,
+	PRIOTITY_LOW=0,
 	PRIOTITY_MED,
 	PRIOTITY_HIGH,
 	PRIOTITY_VERY_HIGH
 }dma_priority;
 typedef enum{
-	MEM_BYTE=1,
+	MEM_BYTE=0,
 	MEM_HALF_WORD,
 	MEM_WORD
 }mem_size;
 typedef enum{
-	PER_BYTE=1,
+	PER_BYTE=0,
 	PER_HALF_WORD,
 	PER_WORD
 }per_size;
 typedef enum{
-	PER_TO_MEM=1,
+	PER_TO_MEM=0,
 	MEM_TO_PER,
 	MEM_TO_MEM
 }dma_direction;
 typedef enum{
-	NORMAL=1,
+	NORMAL=0,
 	CIRCULAR
 }dma_mode;
 typedef struct{
 	DMA_Stream_TypeDef * DMA_STREAM;
 	uint8_t CHANNEL;
-	void *PERIPHERAL_ADDR;
-	void *MEMORY_ADDR;
+	uint32_t *PERIPHERAL_ADDR;
+	uint32_t *MEMORY_ADDR;
 	uint32_t LENGTH;
 	dma_priority PRIORITY;
 	mem_size MEMORY_DATA_SIZE;

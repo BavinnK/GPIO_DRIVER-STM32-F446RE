2f60abf4c716eef0e4be27035122ea46290c0ea3
Bavin <bavinbav5@gmail.com>
2026-01-28 22:40:57 +0300

    forgot to add enable bit of the DMA

--- Core/DRIVERS/dma/MyDMA.c
+++ Core/DRIVERS/dma/MyDMA.c
@@ -62,7 +62,7 @@ static inline void clear_flags(DMA_Stream_TypeDef *stream){
 void DMAx_init(DMA_TypeDef *DMAx,DMA_config_t* DMA_CONFIG){
 	DMA_config(DMAx);
 	DMA_CONFIG->DMA_STREAM->CR&=~(1);//disable stream first
-
+	clear_flags(DMA_CONFIG->DMA_STREAM);//clear flags
 	DMA_CONFIG->DMA_STREAM->CR|=(DMA_CONFIG->CHANNEL<<25)|
 								(DMA_CONFIG->PRIORITY<<16)|
 								(DMA_CONFIG->MEMORY_DATA_SIZE<<13)|
@@ -70,11 +70,11 @@ void DMAx_init(DMA_TypeDef *DMAx,DMA_config_t* DMA_CONFIG){
 								(1<<10)|//Memory address pointer incremented after each data transfer (increment is done according to MSIZE)
 								(DMA_CONFIG->MODE<<8)|
 								(DMA_CONFIG->DIRECTION<<6);
-	DMA_CONFIG->DMA_STREAM->NDTR=DMA_CONFIG->LENGTH-1;//we dont wanna include null terminator
-	DMA_CONFIG->DMA_STREAM->PAR=0;
+
+	DMA_CONFIG->DMA_STREAM->NDTR=DMA_CONFIG->LENGTH;
 	DMA_CONFIG->DMA_STREAM->PAR=(uint32_t)DMA_CONFIG->PERIPHERAL_ADDR;//even if the pointer type is uint_32 the compiler complains so  i had to cast it again
-	DMA_CONFIG->DMA_STREAM->M0AR=0;
 	DMA_CONFIG->DMA_STREAM->M0AR=(uint32_t)DMA_CONFIG->MEMORY_ADDR;
 	DMA_CONFIG->DMA_STREAM->M1AR=0;//SINCE we don't use double buffer mode we will ignore second address register
+	DMA_CONFIG->DMA_STREAM->CR|=1;
 
 }
